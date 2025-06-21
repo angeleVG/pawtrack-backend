09dbb2ec6fe8d2f30a3ccb578eea4dd89ac232a9
angeleVG <a.vangroesen@outlook.com>
2025-06-21 13:10:24 +0200

    add pet model

---
+++ models/Pet.model.js
@@ -0,0 +1,55 @@
+const { Schema, model } = mongoose;
+
+const petSchema = new Schema(
+  {
+    name: 
+    { type: String, required: true },
+    breed: String,
+    birthDate: Date,
+
+    weightHistory: [
+      {
+        date: { type: Date, default: Date.now },
+        weight: { type: Number, required: true }
+      }
+    ],
+
+    medications: [
+      {
+        name: String,
+        purpose: String,
+        dosage: String,
+        startDate: Date,
+        endDate: Date
+      }
+    ],
+
+    vaccinations: [
+  {
+    name: { type: String, required: true }, 
+    date: { type: Date, default: Date.now },  
+nextDueDate: Date  }                       
+],
+
+    food: {
+      brand: String,
+      product: String,
+      portionSize: String,
+      frequency: String,
+      notes: String
+    },
+
+    allergies: [String],
+
+    // relationship between dog and owner
+    owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
+  },
+
+  {
+    // this adds createdAt and updatedAt fields
+    timestamps: true
+  }
+);
+
+const Pet = model("Pet", petSchema);
+module.exports = Pet;
