3f7a1c98124034e5a48b0c0680e7959cb92633cc
angeleVG <a.vangroesen@outlook.com>
2025-07-10 15:28:57 +0200

    align contact enum values in frontend and backend

--- models/Contact.model.js
+++ models/Contact.model.js
@@ -5,6 +5,27 @@ const contactSchema = new Schema({
   pet: { type: mongoose.Schema.Types.ObjectId, ref: "Pet", required: true },
   name: { type: String, required: true },
   phone: { type: String, required: true },
+   relation: {
+    type: String,
+    required: true,
+    enum: [
+      "owner",
+      "vet",
+      "animal ambulance",
+      "groomer",
+      "trainer",
+      "petsitter",
+      "walker",
+      "breeder",
+      "insurance",
+      "emergency contact",
+      "shelter",
+      "boarding",
+      "pharmacy",
+      "other"
+    ],
+    default: "other"
+  },
 }, { timestamps: true });
 
 module.exports = model("Contact", contactSchema);
